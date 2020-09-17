d66127b17a0ff584b734f5dc47494157b75a35d2
Daniel Mireles <danielmireles35@gmail.com>
2020-09-17 10:30:22 -0500

    Delete functions.h

--- functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-bool is_valid_range(int a, int b);
-int count_digit_occurrences(int number, int digit);
-bool has_repeated_digits(int number);
-int count_valid_numbers(int a, int b);
-
-#endif
